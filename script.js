31aab7aeebff1e28019dec65e5a39bbcba6a2abc
Krupa S. <3500324-Krupashanware@users.noreply.replit.com>
2024-07-08 00:52:22 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+//article clicking functionality
+
+/*
+var masalaGuide = $('.masalaGuide');
+var floursAtTheIndianStore = $('.floursAtTheIndianStore');
+
+masalaGuide.on("click", goToMasalaGuide);
+floursAtTheIndianStore.on("click", goToFloursAtTheIndianStore);
+
+
+function goToMasalaGuide() {
+  console.log("Masala Guide");
+  window.location.href = "https://cruisingceliac.krupashanware.repl.co/masalaGuide.html";
+
+}
+
+function goToFloursAtTheIndianStore() {
+  console.log("Flours at the Indian Store");
+  window.location.href = "https://cruisingceliac.krupashanware.repl.co/floursAtTheIndianStore.html";
+}
+
+*/
